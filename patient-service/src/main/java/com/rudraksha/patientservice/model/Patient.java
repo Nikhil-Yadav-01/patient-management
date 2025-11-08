7449f11e213bf651746decb203f5b1b8d4bfe27b
Nikhil Yadav <nikhildevr.01@gmail.com>
2025-11-08 09:47:17 +0530

    Initialization successful

---
+++ patient-service/src/main/java/com/rudraksha/patientservice/model/Patient.java
@@ -0,0 +1,39 @@
+package com.rudraksha.patientservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @Email
+    @NotNull
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate registrationDate;
+}
